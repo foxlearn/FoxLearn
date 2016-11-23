0985ca76514bbf19d896e957b80685c7d9b4dcfb
Lucy <tanhynh@gmail.com>
2016-11-23 22:41:41 +0700

    add licenseinfo.cs

---
+++ FoxLearn.License/FoxLearn.License/LicenseInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxLearn.License
+{
+    public class LicenseInfo
+    {
+        public string FullName { get; set; }
+        public string ProductKey { get; set; }
+    }
+}
